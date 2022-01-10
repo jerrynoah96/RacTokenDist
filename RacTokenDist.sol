3b70c80c8650833406dc52d2c94193cdc8b01e73
jerrynoah96 <newcreationjerry96@gmail.com>
2022-01-10 12:50:24 -0800

    recent update

--- RacTokenDist.sol
+++ RacTokenDist.sol
@@ -21,16 +21,19 @@ contract RacOsTokenDistributor{
 
     mapping(address => uint256) public claimedDateInterval;
 
+    mapping(address => bool) public isWhiteListed;
+
+    mapping(address => bool) public hasClaimedForTheMonth;
 
 
-    event Subscribe(address indexed user, uint256 indexed subTpe);
+
+    event Subscribe(address indexed user, uint256 indexed subType);
     event Claimed(address indexed user, uint256 amountClaimed);
 
-    IERC20 racInstance = IERC20(0x3e3A8476Fb909049a751838552DbfCd21C77D387);
+    IERC20 racInstance = IERC20(0xFAC774043c5786e0f97De5FEDcFE4F2a175626b0);
 
-    address admin;
-    // blackList mapping check
-    mapping(address => bool) public isBlackListed;
+    address admin =  0x134d255b124eAAb8C9790EdF10E0Cd603ff22b55;
+   
 
     modifier onlyAdmin(){
         require(msg.sender == admin, "only admin can do this");
@@ -38,45 +41,51 @@ contract RacOsTokenDistributor{
     }
 
     // takes in uint256 as subscription type- this will range only from 1-3
-    function subscribe(uint256 _subscriptionType) public {
-        require(isBlackListed[msg.sender] == false, "you have been blackListed, kindly reach out to the admin if you think something is wrong");
-        require(_subscriptionType == 1 || _subscriptionType == 2 || _subscriptionType == 3, "number passed not within subscription range");
-        subscriptionType[msg.sender] =_subscriptionType;
+    function batchWhitelist(address[] memory _users, uint256[] memory _subscriptionType) public onlyAdmin {
+        require(_users.length == _subscriptionType.length, "users and subscriptionType lenght mismatch");
+        for(uint256 i=0; i < _users.length; i++ ){
+        require(_subscriptionType[i] == 1 || _subscriptionType[i] == 2 || _subscriptionType[i] == 3, "number passed not within subscription range");
+        subscriptionType[_users[i]] =_subscriptionType[i];
+        isWhiteListed[_users[i]] = true;
+        hasClaimedForTheMonth[_users[i]] = false;
 
-        emit Subscribe(msg.sender, _subscriptionType);
+        emit Subscribe(_users[i], _subscriptionType[i]);
+        }
+         
     }
 
-    function setClaimableTokenPerSub(uint256 subType, uint256 _amountClaimable) public{
+    function setClaimableTokenPerSub(uint256 subType, uint256 _amountClaimable) public onlyAdmin{
         
         claimableTokensPerSubType[subType] = _amountClaimable;
     }
 
-    //admin can choose
-    function blackList(address _user) public onlyAdmin{
-        isBlackListed[_user] = true;
+    
+    function removeUsers(address[] memory _usersToRemove) public onlyAdmin{
+        for(uint i=0; i<_usersToRemove.length; i++){
+            isWhiteListed[_usersToRemove[i]] = false;
+            subscriptionType[_usersToRemove[i]] = 0;
+        }
+        
     }
 
-    // admin can remove an address from blackList
-    function removeFromBlackList(address _user) public {
-        require(isBlackListed[_user] == true, "this address is currently not blackListed");
-        isBlackListed[_user] = false;
-    }
+    
 
-
-    function claimRacForTheMonth() public{
+    function claimRacForTheMonth() public {
+        require(isWhiteListed[msg.sender] == true, "you are currently not part of the whiteListed addresses");
         require(subscriptionType[msg.sender] == 1 || subscriptionType[msg.sender] == 2 || subscriptionType[msg.sender] == 3, "You do not have a valid subscription on this platform");
-        require(isBlackListed[msg.sender] == false, "you are currently blackListed from claiming, Please contact admin");
-        require(block.timestamp.sub(claimedDateInterval[msg.sender]) >= 120, "Not yet time");
-
+        require(hasClaimedForTheMonth[msg.sender] == false, "you have claimed already, kindly wait to be whiteListed for another round");
+        
         //check user sub type to determine how much token to claimable
         uint256 userSubType = subscriptionType[msg.sender];
 
-        // update time for user
-        claimedDateInterval[msg.sender] = block.timestamp.add(120);
+        // change mapping to true that address has claimedDateInterval
+        hasClaimedForTheMonth[msg.sender] = true;
+
+        // change subscription type to 0
+        subscriptionType[msg.sender] = 0;
 
         // use userSubType to check how much claimableTokensPerSub token
         racInstance.transfer(msg.sender, claimableTokensPerSubType[userSubType]);
-
 
         emit Claimed(msg.sender, claimableTokensPerSubType[userSubType]);
 
